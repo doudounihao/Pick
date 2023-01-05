5680303ac7f854bf037a493ce49e46a2a437c679
haokai <haokai.quanqiuwa.com>
2023-01-05 14:26:58 +0800

    功能优化

--- app/src/main/java/com/app/qqwpick/data/home/ThirdGoodsBean.kt
+++ app/src/main/java/com/app/qqwpick/data/home/ThirdGoodsBean.kt
@@ -1,8 +1,26 @@
 package com.app.qqwpick.data.home
 
+import com.app.qqwpick.util.MathUtils
+import java.io.Serializable
+
 data class ThirdGoodsBean(
     val commodityName: String,
     val originPrice: String,
     val num: String,
+    val adjustNum: String,
     val commodityPic: String
-)
+) : Serializable {
+
+    @JvmName("getSinglePrice")
+    fun getSinglePrice(): String {
+        try {
+            return if (0.equals(MathUtils.add(num, adjustNum))) {
+                MathUtils.divide(originPrice, if (num.equals("0")) "1" else num)
+            } else {
+                MathUtils.divide(originPrice, MathUtils.add(num, adjustNum))
+            }
+        } catch (e: Exception) {
+        }
+        return "0"
+    }
+}
